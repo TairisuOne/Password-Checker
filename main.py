c628005ae14addcdc4f4548159432acd6fc2a000
Tyrese <tairisu.stewart@gmail.com>
2025-09-20 22:53:06 -0700

    added an if statement to check if the password contains: at least one uppercase letter

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ password = input("Enter a password: ")
 
 if len(password) < 8:
     print("Weak Password: too short")
+
+if any(char.isupper() for char in password):
+    print("Password Contains at least 1 uppercase letter")
 print(password)
