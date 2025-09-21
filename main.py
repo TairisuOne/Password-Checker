47120700aafb351e9755ed2f1dbca7435c816dc8
Tyrese <tairisu.stewart@gmail.com>
2025-09-20 23:00:17 -0700

    added to new if statements to check for lowercase letters and numbers in the password

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 
+
 password = input("Enter a password: ")
 
 if len(password) < 8:
@@ -7,4 +8,11 @@ if len(password) < 8:
 
 if any(char.isupper() for char in password):
     print("Password Contains at least 1 uppercase letter")
+
+if any(char.islower() for char in password):
+    print(True)
+
+if any(char.isdigit() for char in password):
+    print(True)
+
 print(password)
