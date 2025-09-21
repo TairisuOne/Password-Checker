87a54aad4fcc8c4a6298bfdb0d79911320033a1a
Tyrese <tairisu.stewart@gmail.com>
2025-09-21 14:20:05 -0700

    minor comments were added explaining certain snippets of code

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
-special_char = "!@#$%^&*()<>?,.:;"
+special_char = "!@#$%^&*()<>?,.:;" # variable that hold special characters that need to be present in password
 
-password = input("Enter a password to check strength: ")
+password = input("Enter a password to check strength: ") # asks for the user input - regarding password
 
-def password_checker(password):
+def password_checker(password): # reusable code that can be called upon 
 
     issues = []
 
-    has_uppercase = any(char.isupper() for char in password)
+    has_uppercase = any(char.isupper() for char in password) # checks the password for any uppercase letter if any is found it returns True
 
     has_lowercase = any(char.islower() for char in password)
 
@@ -15,7 +15,7 @@ def password_checker(password):
 
     has_special = any(char in special_char for char in password)
 
-    if len(password) < 8:
+    if len(password) < 8: 
         issues.append("Weak Password: too short. Must be a minimum of 8 characters long.")
 
     if not has_uppercase:
@@ -39,5 +39,5 @@ strength, issues = password_checker(password)
 
 print("Strenght: ", strength)
 
-for x in issues:
+for x in issues: # a loop that runs thru issues one at a time
     print("Suggestions - ", x)
