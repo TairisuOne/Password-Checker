d6a9686b78dcb23fc10b45e01b43ab9ae94a4e3f
Tyrese <tairisu.stewart@gmail.com>
2025-09-21 01:30:35 -0700

    incoporated a function to make code reuseable

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ special_char = "!@#$%^&*()<>?,.:;"
 
 password = input("Enter a password to check strength: ")
 
-has_uppercase = any(char.isupper() for char in password)
+def password_checker(password):
 
-has_lowercase = any(char.islower() for char in password)
+    has_uppercase = any(char.isupper() for char in password)
 
-has_digit = any(char.isdigit() for char in password)
+    has_lowercase = any(char.islower() for char in password)
 
-has_special = any(char in special_char for char in password)
+    has_digit = any(char.isdigit() for char in password)
 
-if len(password) < 8:
-    print("Weak Password: too short")
+    has_special = any(char in special_char for char in password)
 
-elif has_uppercase and has_lowercase and has_digit and has_special and len(password) >= 8:
-    print("Strong Password: password meets all criteria")
+    if len(password) < 8:
+        return"Weak Password: too short"
+
+    elif has_uppercase and has_lowercase and has_digit and has_special and len(password) >= 8:
+        return "Strong Password: password meets all criteria"
     
-print(password)
+print(password_checker(password))
